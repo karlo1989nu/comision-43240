63d84c13821d9de1032b08feb60282f2ff97a80c
unknown <karlo1989nu@gmail.com>
2023-06-09 01:20:11 +0200

    primera parte classe 2 componentes, se crea en nueva rama

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { Navbar } from "./components/layout/navbar/Navbar";
+import { ProductsList } from "./components/pages/itemlistcontainer/ItemListContainer";
 
 const App = () => {
+  const [montado, setMontado] = useState(false);
+  const [nombre, setNombre] = useState("Karlo");
   return (
     <>
       <nav style={{ margin: "30px" }}>
         {" "}
         <Navbar />
       </nav>
+      {montado && <ProductsList nombre={nombre} />}
+      <button onClick={() => setMontado(!montado)}>Montar</button>
+      <button onClick={() => setNombre("Pepe")}>Name</button>
     </>
   );
 };
--- src/components/pages/itemlistcontainer/ItemListContainer.jsx
+++ src/components/pages/itemlistcontainer/ItemListContainer.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+export const ProductsList = ({ nombre }) => {
+  const [contador, setContador] = useState(0);
+  console.log("El componente se actualiza");
 
-export const ProductsList = () => {
- const [contador, setContador] = useState(0 )
- console.log('El componente fue montado');
+  useEffect(() => {
+    console.log("API request");
+  }, [nombre]);
+  console.log("abajo");
 
   return (
-   <>
-   <h2>Aquí van los productos</h2>
+    <>
+      <h2>Aquí van los productos</h2>
 
-<button onClick={() => setContador(contador+1)}>Sumar</button>
-   {/*<Counter/>*/}
-   </>
-      
+      <button onClick={() => setContador(contador + 1)}>Sumar</button>
+      {/*<Counter/>*/}
+    </>
   );
 };
