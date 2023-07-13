e9f12abed0c8d8fc81e51fb894772a10b942bca7
unknown <karlo1989nu@gmail.com>
2023-07-13 18:25:54 +0200

    inicia clase

--- src/components/pages/formFormik/FormFormik.jsx
+++ src/components/pages/formFormik/FormFormik.jsx
@@ -1,12 +1,42 @@
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const FormFormik = () => {
+  const { handleSubmit, handleChange } = useFormik({
+    initialValues: { nombre: "", email: "", telefono: "" },
+    onSubmit: (datos) => {
+      console.log("Formulario enviado");
+      console.log(datos);
+    },
+    validationSchema: Yup.object({
+      nombre: Yup.string().required(),
+    }),
+  });
   return (
-    <div>
-      <form>
-        <TextField label="Nombre" variant="outlined" />
-        <TextField label="E-mail" variant="outlined" />
-        <TextField label="Telefono" variant="outlined" />
+    <div style={{ padding: "40px" }}>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="Nombre"
+          variant="outlined"
+          name="nombre"
+          onChange={handleChange}
+        />
+        <TextField
+          label="E-mail"
+          variant="outlined"
+          name="email"
+          onChange={handleChange}
+        />
+        <TextField
+          label="Telefono"
+          variant="outlined"
+          name="telefono"
+          onChange={handleChange}
+        />
+        <Button variant="contained" size="small" type="submit">
+          Submit
+        </Button>
       </form>
     </div>
   );
