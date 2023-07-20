8b603d83e8fe751e93180f19b1fe3e741cea3036
unknown <karlo1989nu@gmail.com>
2023-07-20 17:19:37 +0200

    primera parte clase 13

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,5 +1,39 @@
-const Checkout = () => {
-  return <div>Checkout</div>;
+import { Button, TextField } from "@mui/material";
+
+const Checkout = ({ handleChange, handleSubmit, errors }) => {
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="Name"
+          variant="outlined"
+          name="name"
+          onChange={handleChange}
+          helperText={errors.name}
+          error={errors.name ? true : false}
+        />
+        <TextField
+          label="Email"
+          variant="outlined"
+          name="email"
+          onChange={handleChange}
+          helperText={errors.email}
+          error={errors.email ? true : false}
+        />
+        <TextField
+          label="Phone"
+          variant="outlined"
+          name="phone"
+          onChange={handleChange}
+          helperText={errors.phone}
+          error={errors.phone ? true : false}
+        />
+        <Button variant="contained" type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default Checkout;
--- src/components/pages/checkout/CheckoutContainer.jsx
+++ src/components/pages/checkout/CheckoutContainer.jsx
@@ -1,7 +1,31 @@
+import { useFormik } from "formik";
 import Checkout from "./Checkout";
+import * as Yup from "yup";
 
 const CheckoutContainer = () => {
-  return <Checkout />;
+  const { handleSubmit, handleChange, errors } = useFormik({
+    initialValues: { name: "", email: "", phone: "" },
+    onSubmit: (data) => {
+      console.log(data);
+    },
+    validateOnChange: false,
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .required("Required")
+        .min(2, "Name must be min 2 characters long"),
+      email: Yup.string().required("Required").email("Must be a valid email"),
+      phone: Yup.string()
+        .required("Required")
+        .min(6, "Enter a valid phone number"),
+    }),
+  });
+  return (
+    <Checkout
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+      errors={errors}
+    />
+  );
 };
 
 export default CheckoutContainer;
